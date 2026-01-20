310aadeff975e9b53dedff560cb4376446c9f0db
kaneezfatimh2005-sketch <kaneezfatimh2005@gmail.com>
2026-01-20 13:19:04 +0530

    as

--- tourism/urls.py
+++ tourism/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search_place, name='search_place'),
+     path('place/head/',views.head, name='head' ),
+    path('suggest/', views.suggest_places, name='suggest_places'),
+
     path('place/<slug:slug>/', views.place_detail, name='place_detail'),
+   
 ]
 
--- tourism/views.py
+++ tourism/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Place
+from django.http import JsonResponse
 
 def home(request):
     places = Place.objects.all()
@@ -14,3 +15,14 @@ def search_place(request):
 def place_detail(request, slug):
     place = get_object_or_404(Place, slug=slug)
     return render(request, "place_detail.html", {"place": place})
+
+def head(request):
+    return render(request, "head.html")
+def suggest_places(request):
+    query = request.GET.get("q", "").strip()
+    if query:
+        Places=Place.objects.filter(name__icontains=query)[:8]
+        suggestions=[p.name for p in Places]
+    else:
+        suggestions=[]
+    return JsonResponse( suggestions,safe=False)
